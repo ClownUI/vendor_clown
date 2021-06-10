051f8e2db261a25884990db98c4a6c1c8480e4c2
Sebastiano Barezzi <barezzisebastiano@gmail.com>
2021-06-11 00:39:41 +0200

    soong: Add TARGET_CAMERA_BOOTTIME_TIMESTAMP conditional
    Change-Id: Ic48acda208fc9990fc5c9159ad06518d48f21477
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- config/BoardConfigSoong.mk
+++ config/BoardConfigSoong.mk
@@ -34,6 +34,7 @@ SOONG_CONFIG_clownGlobalVars += \
     disable_bluetooth_le_set_host_feature \
     gralloc_handle_has_reserved_size \
     target_init_vendor_lib \
+    needs_camera_boottime \
     camera_needs_client_info \
     target_inputdispatcher_skip_event_key \
     target_ld_shim_libs \
@@ -66,7 +67,7 @@ SOONG_CONFIG_clownGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_clownGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 SOONG_CONFIG_clownGlobalVars_uses_oppo_camera := $(TARGET_USES_OPPO_CAMERA)
 SOONG_CONFIG_clownGlobalVars_camera_needs_client_info := $(TARGET_CAMERA_NEEDS_CLIENT_INFO)
-
+SOONG_CONFIG_clownGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_TIMESTAMP)
 
 # Set default values
 TARGET_ADDITIONAL_GRALLOC_10_USAGE_BITS ?= 0
