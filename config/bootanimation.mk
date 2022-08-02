2b23847c553ae6195d8a9058ed33ff2daf857e03
jhenrique09 <jhsv09@gmail.com>
2022-08-02 00:11:05 +0000

    vendor: Nuke 720p bootanimation
    Change-Id: I77deebdfbf6de89ce3e881c5f45285fb9497b3bb
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- config/bootanimation.mk
+++ config/bootanimation.mk
@@ -1,10 +1,7 @@
 # Boot Animation
 
-# 720p
-ifeq ($(TARGET_BOOT_ANIMATION_RES),720)
-PRODUCT_COPY_FILES += vendor/clown/prebuilt/common/bootanimation/720.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 # 1080p
-else ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
+ifeq ($(TARGET_BOOT_ANIMATION_RES),1080)
 PRODUCT_COPY_FILES += vendor/clown/prebuilt/common/bootanimation/bootanimation_1080.zip:$(TARGET_COPY_OUT_PRODUCT)/media/bootanimation.zip
 # 1440p
 else ifeq ($(TARGET_BOOT_ANIMATION_RES),1440)
