e49fd64f791684656eca4fb67e04d962cf4df8bb
someone5678 <nemui3353@gmail.com>
2022-11-25 14:29:55 +0900

    vendor: Build BtHelper
    Change-Id: Ic196ffe2a59de2a189478da16b7ba1b5a7393e45
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -208,6 +208,10 @@ PRODUCT_PACKAGES += \
 PRODUCT_COPY_FILES += \
     vendor/clown/prebuilt/common/etc/init/init.clown-updater.rc:$(TARGET_COPY_OUT_SYSTEM_EXT)/etc/init/init.clown-updater.rc
 
+# BtHelper
+PRODUCT_PACKAGES += \
+    BtHelper
+    
 # Config
 PRODUCT_PACKAGES += \
     SimpleDeviceConfig \
