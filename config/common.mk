d7a5d3f120bfc53f5f7a2795dbbc360a045e8bde
nift4 <nift4@protonmail.com>
2023-04-14 00:37:39 +0200

    vendor: Remove useless addon.d file
    * Avoid detection as custom ROM
    
    Change-Id: I1fbd63cc116d53c7c366662b632163f6bc079bbf
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- config/common.mk
+++ config/common.mk
@@ -31,11 +31,7 @@ endif
 # Backup Tool
 PRODUCT_COPY_FILES += \
     vendor/clown/prebuilt/common/bin/backuptool.sh:install/bin/backuptool.sh \
-    vendor/clown/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions \
-    vendor/clown/prebuilt/common/bin/50-clown.sh:$(TARGET_COPY_OUT_SYSTEM)/addon.d/50-clown.sh
-
-PRODUCT_ARTIFACT_PATH_REQUIREMENT_ALLOWED_LIST += \
-    system/addon.d/50-clown.sh
+    vendor/clown/prebuilt/common/bin/backuptool.functions:install/bin/backuptool.functions
 
 ifneq ($(strip $(AB_OTA_PARTITIONS) $(AB_OTA_POSTINSTALL_CONFIG)),)
 PRODUCT_COPY_FILES += \
@@ -72,7 +68,7 @@ PRODUCT_COPY_FILES += \
 # Component overrides
 PRODUCT_PACKAGES += \
     clown-component-overrides.xml
-    
+
 # Face Unlock
 TARGET_FACE_UNLOCK_SUPPORTED ?= $(TARGET_SUPPORTS_64_BIT_APPS)
 
