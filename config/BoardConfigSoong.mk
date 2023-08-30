PixelExtended/vendor_aosp

soong: extend support for oppo camera (#14)

config/BoardConfigSoong.mk
@@ -40,6 +40,7 @@ SOONG_CONFIG_customGlobalVars += \
     uses_legacy_fd_fbdev \
     uses_egl_display_array \
     uses_oplus_camera \
+    uses_oppo_camera \
     uses_nothing_camera
 
 SOONG_CONFIG_NAMESPACES += customNvidiaVars
@@ -64,6 +65,7 @@ SOONG_CONFIG_customGlobalVars_needs_camera_boottime := $(TARGET_CAMERA_BOOTTIME_
 SOONG_CONFIG_customGlobalVars_uses_egl_display_array := $(TARGET_USES_EGL_DISPLAY_ARRAY)
 SOONG_CONFIG_customGlobalVars_target_uses_prebuilt_dynamic_partitions := $(TARGET_USES_PREBUILT_DYNAMIC_PARTITIONS)
 SOONG_CONFIG_customGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
+SOONG_CONFIG_customGlobalVars_uses_oppo_camera := $(TARGET_USES_OPPO_CAMERA)
 SOONG_CONFIG_customGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
 SOONG_CONFIG_customNvidiaVars_uses_nvidia_enhancements := $(NV_ANDROID_FRAMEWORK_ENHANCEMENTS)
 SOONG_CONFIG_customQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_ENABLED_EXTENDED_COMPRESS_FORMAT)

ClownUI/vendor_clown

Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

config/BoardConfigSoong.mk
@@ -42,6 +42,7 @@ SOONG_CONFIG_clownGlobalVars += \
     target_trust_usb_control_disable \
     uses_egl_display_array \
     uses_oplus_camera \
+    uses_oppo_camera \
     uses_nothing_camera
 
 SOONG_CONFIG_NAMESPACES += clownQcomVars
@@ -62,7 +63,7 @@ SOONG_CONFIG_clownQcomVars_supports_extended_compress_format := $(AUDIO_FEATURE_
 SOONG_CONFIG_clownQcomVars_uses_pre_uplink_features_netmgrd := $(TARGET_USES_PRE_UPLINK_FEATURES_NETMGRD)
 SOONG_CONFIG_clownGlobalVars_uses_oplus_camera := $(TARGET_USES_OPLUS_CAMERA)
 SOONG_CONFIG_clownGlobalVars_uses_nothing_camera := $(TARGET_USES_NOTHING_CAMERA)
-
+SOONG_CONFIG_clownGlobalVars_uses_oppo_camera := $(TARGET_USES_OPPO_CAMERA)
 
 
 # Set default values