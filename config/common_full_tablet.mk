LineageOS/android_vendor_lineage

config: tablet: Enable freeform and large screen support for all targets
Change-Id: I8137911f96cf2eb347d8e5012be649ddb01057f8

config/common_full_tablet.mk
@@ -1,7 +1,7 @@
 # Inherit mobile full common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_mobile_full.mk)
 
-# Inherit full tablet common Lineage stuff
-$(call inherit-product, vendor/lineage/config/full_tablet.mk)
+# Inherit tablet common Lineage stuff
+$(call inherit-product, vendor/lineage/config/tablet.mk)
 
 $(call inherit-product, vendor/lineage/config/telephony.mk)

config/common_full_tablet_wifionly.mk
@@ -1,7 +1,7 @@
 # Inherit mobile full common Lineage stuff
 $(call inherit-product, vendor/lineage/config/common_mobile_full.mk)
 
-# Inherit full tablet common Lineage stuff
-$(call inherit-product, vendor/lineage/config/full_tablet.mk)
+# Inherit tablet common Lineage stuff
+$(call inherit-product, vendor/lineage/config/tablet.mk)
 
 $(call inherit-product, vendor/lineage/config/wifionly.mk)

ClownUI/vendor_clown

Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

config/common_full_tablet.mk
@@ -1,7 +1,7 @@
 # Inherit mobile full common stuff
 $(call inherit-product, vendor/clown/config/common_mobile_full.mk)
 
-# Inherit full tablet common stuff
-$(call inherit-product, vendor/clown/config/full_tablet.mk)
+# Inherit tablet common stuff
+$(call inherit-product, vendor/clown/config/tablet.mk)
 
 $(call inherit-product, vendor/clown/config/telephony.mk)

config/common_full_tablet_wifionly.mk
@@ -1,7 +1,7 @@
 # Inherit mobile full common stuff
 $(call inherit-product, vendor/clown/config/common_mobile_full.mk)
 
-# Inherit full tablet common stuff
-$(call inherit-product, vendor/clown/config/full_tablet.mk)
+# Inherit tablet common stuff
+$(call inherit-product, vendor/clown/config/tablet.mk)
 
 $(call inherit-product, vendor/clown/config/wifionly.mk)
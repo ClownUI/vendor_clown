c50deb7689f61f2230d50d394e85d964b5f8795f
Sebastiano Barezzi <seba@sebaubuntu.dev>
2024-07-21 22:01:35 +0200

    config: tablet: Enable freeform and large screen support for all targets
    Change-Id: I8137911f96cf2eb347d8e5012be649ddb01057f8
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- config/common_full_tablet_wifionly.mk
+++ config/common_full_tablet_wifionly.mk
@@ -1,7 +1,7 @@
 # Inherit mobile full common stuff
 $(call inherit-product, vendor/clown/config/common_mobile_full.mk)
 
-# Inherit full tablet common stuff
-$(call inherit-product, vendor/clown/config/full_tablet.mk)
+# Inherit tablet common stuff
+$(call inherit-product, vendor/clown/config/tablet.mk)
 
 $(call inherit-product, vendor/clown/config/wifionly.mk)
