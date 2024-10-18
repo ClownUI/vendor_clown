4b5beb8618b337ac64019aaf5e05ee5ad91f45ee
Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>
2024-10-18 08:34:03 +0000

    Revert "clown: Inherit Pixel additions via `WITH_GMS`"
    This reverts commit a13df487900fc9843be99dc23480f03f01027183.
    
    Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

--- config/common_full_tablet.mk
+++ config/common_full_tablet.mk
@@ -5,15 +5,3 @@ $(call inherit-product, vendor/clown/config/common_mobile_full.mk)
 $(call inherit-product, vendor/clown/config/tablet.mk)
 
 $(call inherit-product, vendor/clown/config/telephony.mk)
-
-# GMS
-WITH_GMS ?= true
-ifeq ($(WITH_GMS),true)
-ifeq ($(TARGET_USES_MINI_GAPPS),true)
-$(call inherit-product, vendor/gms/gms_mini.mk)
-else ifeq ($(TARGET_USES_PICO_GAPPS),true)
-$(call inherit-product, vendor/gms/gms_pico.mk)
-else
-$(call inherit-product, vendor/gms/gms_full.mk)
-endif
-endif
