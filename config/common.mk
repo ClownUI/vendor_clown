bananadroid/android_vendor_banana

vendor: Add preinstalled package list to stop auto cloning
Only extra adds needed, gms now ships it's own lists for this.

config/common.mk
@@ -67,6 +67,10 @@ PRODUCT_COPY_FILES += \
     vendor/banana/prebuilt/common/etc/permissions/privapp-permissions-livedisplay.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-livedisplay.xml \
     vendor/banana/prebuilt/common/etc/permissions/privapp-permissions-custom.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/permissions/privapp-permissions-custom.xml
 
+# Cloned app exemption
+PRODUCT_COPY_FILES += \
+    vendor/banana/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-custom-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-custom-product.xml
+
 # Include Banana extra packages
 include vendor/banana/config/packages.mk
 include vendor/banana/config/prop.mk

ClownUI/vendor_clown

Signed-off-by: Pranav Vashi <neobuddy89@gmail.com>
Signed-off-by: Edwiin Kusuma Jaya <kutemeikito0905@gmail.com>

config/common.mk
@@ -72,6 +72,10 @@ endif
 PRODUCT_PRODUCT_PROPERTIES += \
     ro.launcher.blur.appLaunch=0
     
+# Cloned app exemption
+PRODUCT_COPY_FILES += \
+    vendor/clown/prebuilt/common/etc/sysconfig/preinstalled-packages-platform-clown-product.xml:$(TARGET_COPY_OUT_PRODUCT)/etc/sysconfig/preinstalled-packages-platform-clown-product.xml
+
 # Disable async MTE on system_server
 PRODUCT_SYSTEM_EXT_PROPERTIES += \
     arm64.memtag.process.system_server=off